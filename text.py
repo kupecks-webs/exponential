4e8034d2228cac604dcf61280469e2325d8d4bf0
kupeck <70723820+kupeck@users.noreply.github.com>
2020-10-28 23:31:30 +0100

    Changer generator to python

---
+++ text.py
@@ -0,0 +1,12 @@
+i = 0
+while(i < 100):
+    i = i + 1
+    print('</tr><tr>')
+    print('   <td id="2-' + str(i) + '"></td>')
+    print('   <td id="3-' + str(i) + '"></td>')
+    print('   <td id="4-' + str(i) + '"></td>')
+    print('   <td id="5-' + str(i) + '"></td>')
+    print('   <td id="6-' + str(i) + '"></td>')
+    print('   <td id="7-' + str(i) + '"></td>')
+    print('   <td id="8-' + str(i) + '"></td>')
+    print('   <td id="9-' + str(i) + '"></td>')
