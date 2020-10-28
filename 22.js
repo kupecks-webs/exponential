0bf0933aa55fc274db25847eadbf39f8495f2698
kupeck <70723820+kupeck@users.noreply.github.com>
2020-10-28 23:54:26 +0100

    added a table

---
+++ 22.js
@@ -0,0 +1,10 @@
+console.log("22.js loaded sucessfully!")
+document.getElementById("2-" + "1").innerHTML = 4
+for(var i = 1; i < 100; i++) {
+    var ii= i + 1
+    var id= "2-" + i
+    var dd= "2-" + ii
+    var x = document.getElementById(id).innerHTML
+    document.getElementById(dd).innerHTML = pow(x, 2)
+    console.log(i + "done!")
+}
