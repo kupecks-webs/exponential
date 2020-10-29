d2fa19e2ca6b4fc83ebac2c1e96fe8d7f28895c6
kupeck <70723820+kupeck@users.noreply.github.com>
2020-10-29 14:28:30 +0100

    something

--- 22.js
+++ 22.js
@@ -1,5 +1,8 @@
 console.log("22.js loaded sucessfully!")
 document.getElementById("2-" + "1").innerHTML = 4
+function pow(p1, p2) {
+    return p1 ** p2
+}
 for(var i = 1; i < 100; i++) {
     var ii= i + 1
     var id= "2-" + i
